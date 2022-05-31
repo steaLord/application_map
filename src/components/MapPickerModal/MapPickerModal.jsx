364c6ba09f35c5fb600fc634def47e2db36f3cf4
Данияр Сулейменов <stealord@MacBook-Pro-Daniar.local>
2022-05-31 08:56:04 +0600

    hotfix

--- src/components/MapPickerModal/MapPickerModal.jsx
+++ src/components/MapPickerModal/MapPickerModal.jsx
@@ -5,32 +5,15 @@ import "./MapPickerModal.css";
 const MapPickerModal = () => {
   return (
     <div onClick={appStore.mapModalToggle} className="modal_map_bg">
-      <div onClick={(e) => e.stopPropagation()}>
-        <Map
-          locos={[]}
-          setDetailsId={null}
-          mapPicking={true}
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBu71A-nLwXyRHb-crM1QBzCfADFdyLwKE"
-          loadingElement={
-            <div
-              onClick={(e) => e.stopPropagation()}
-              style={{ height: `100%` }}
-            />
-          }
-          containerElement={
-            <div
-              onClick={(e) => e.stopPropagation()}
-              className="modal_map_window"
-            ></div>
-          }
-          mapElement={
-            <div
-              onClick={(e) => e.stopPropagation()}
-              style={{ height: `100%` }}
-            />
-          }
-        />
-      </div>
+      <Map
+        locos={[]}
+        setDetailsId={null}
+        mapPicking={true}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBu71A-nLwXyRHb-crM1QBzCfADFdyLwKE"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div className="modal_map_window"></div>}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 };
